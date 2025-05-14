45e2cd8ad98a99c54cac2165c00a1640a2cb5add
Sakumo21 <moaud.fdofus@gmail.com>
2025-05-14 14:42:30 +0100

    cpp07/ex00: completed!

---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,32 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <sstream>
+#include <iomanip>
+
+template <typename T> void swap(T &a, T &b)
+{
+    T tmp;
+    tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename T> T min(T &a, T &b)
+{
+    if (a < b)
+        return a;
+    return b;
+}
+
+template <typename T> T max(T &a, T &b)
+{
+    if (a > b)
+        return a;
+    return b;
+}
+
+#endif
